315ac114b115b521f412011b8a6bcde033ef705f
ydaniju <yusuf.daniju@andela.com>
2017-12-30 15:59:06 +0100

    succesfully display first name and last name

--- db/seeds.cr
+++ db/seeds.cr
@@ -1,10 +1,4 @@
-n = 0
-loop do
-  n += 1
-  user = User.new
-  user.first_name = "Yusuf"
-  user.last_name = "Daniju"
-  user.save
-  puts "created"
-  break if n == 5
-end
+user = User.new
+user.first_name = "Yusuf"
+user.last_name = "Daniju"
+user.save
